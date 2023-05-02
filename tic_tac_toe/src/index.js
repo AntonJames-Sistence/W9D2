e08c2c5588b8c5918533e9256b989252b60a3b2e
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-05-02 13:51:04 -0400

    bundle setup

--- tic_tac_toe/src/index.js
+++ tic_tac_toe/src/index.js
@@ -1,4 +1,8 @@
-Add your import statements for View and Game here
+// Add your import statements for View and Game here
+const Game = require('../ttt_node/game.js');
+const View = require('./ttt-view.js');
+const newGame = new Game();
+// newGame.start();
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
