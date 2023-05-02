1729fcf2dfe6cd00bcbfe9ee53bea56f104f01ec
lewiskk016 <lewiskk016@gmail.com>
2023-05-02 15:40:12 -0400

    got square

--- tic_tac_toe/src/ttt-view.js
+++ tic_tac_toe/src/ttt-view.js
@@ -6,27 +6,37 @@ class View {
     this.setupBoard();
     // debugger
   }
-  
+
   setupBoard() {
     // const ttt = document.querySelector(".ttt");
     let ul = document.createElement("ul");
     // debugger
     this.el.appendChild(ul);
-    
+
     // ul.appendChild(li)
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
       let li = document.createElement("li");
+      let pos = [i, j];
+      li.dataset.position = [pos]
       ul.appendChild(li);
-      li.appendChild(document.createTextNode(i));
+      // li.appendChild(document.createTextNode([i , j]));
+      };
     };
   }
-  
+
   handleClick(e) {
+    this.el.addEventListener('click', (e) => {
+      e.preventDefault();
+      this.game.protoype.playMove([0,0]);
+    })
   }
 
   makeMove(square) {
+    // let element = e.target
+    // element.classList.toggle("done")
   }
-  
+
   handleGameOver() {
   }
 }
